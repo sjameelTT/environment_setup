cc13feda2074cb25b72c1036a6bb7c7f93ea7ab5
Saad Jameel <sjameel@tenstorrent.com>
2024-03-19 15:54:34 +0000

    Add my zsh, tmux, helix and git configs, as well as an environment setup script

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# environment_setup
+# Saad's UNIX (DEBIAN) environment
+
+oh-my-zsh with autosuggestions, autojump, fzf-zsh, syntax hightlighting
+
+powerlevel10k theme set up (interactive)
+
+tmux and tpm with dracula theme and mouse mode on (set -g mouse on)
+
+cargo installation
+
+helix via cargo along with my configurations
+
+delta as git pager
+
+global git config settings
+
+eza via cargo
+
+ripgrep via cargo
+
+bat/batcat via cargo
+
+fdfind via cargo
---
+++ setup_env.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+dir=$PWD
+
+# install oh my zsh and activate it
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+source ~/.zshrc
+
+# add zsh-autosuggestions, syntax highlighting
+cd ~/.oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+
+# add autojump
+cd $(dir)
+git clone git://github.com/wting/autojump.git
+cd autojump
+./install.py
+
+# add fzf-zsh plugin and powerlevel10k
+git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# copy over .zshrc and reload it, it will configure powerlevel10k
+cd $(dir)
+cp ./.zshrc ~
+omz reload
+
+# start tmux session, install tpm and add/reload tmux configuration
+tmux new
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cd $(dir)
+cp ./.tmux.conf ~
+tmux source ~/.tmux.conf
+
+# install cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# install helix
+cd $(dir)
+git clone https://github.com/helix-editor/helix
+cd helix
+cargo install --path helix-term --locked
+cp config.toml ~/.config/helix/config.toml
+
+# install delta and config git
+cargo install git-delta
+cp ./.gitconfig ~
+
+# install eza
+cargo install eza
+
+# install ripgrep
+cargo install ripgrep
+
+# install bat
+cargo install --locked bat
+
+# install fdfind
+cargo install fd-find
